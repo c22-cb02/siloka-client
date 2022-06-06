115bfdbc9668f77eabe45d0752a6fb054b236d56
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2022-06-06 12:54:54 +0700

    feat(settings): init settings activity

---
+++ app/src/main/java/com/siloka/client/views/settings/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.siloka.client.views.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
