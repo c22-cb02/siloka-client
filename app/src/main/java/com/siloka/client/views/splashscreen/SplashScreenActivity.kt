9bd6b74a9d6ae9846191369e1b6a9c42c1c027e3
Fahdii Ajmalal Fikrie <47189456+fahdikrie@users.noreply.github.com>
2022-06-06 10:56:22 +0700

    Implement Splash Screen UI (#2)
    * build: enable viewbindingding
    
    * build: add basic deps
    
    Retrofit, MVVM, Coroutines, etc.
    
    * build: connect app to firebase
    
    * build: setup firebase app distribution
    
    * build: add Github Actions to FAD integration yml
    
    * build(android.yml): bump actions/ checkout & setup-java to v3
    
    * build(android.yml): downgrade actions/ checkout & setup-java back to v1
    
    * build(android.yml): change JDK version to 11
    
    * feat: init Main & SplashScreen activity
    
    * chore: add Museo Sans fonts
    
    * chore: set SplashScreenActivity as the main activity
    
    * feat(splash-screen): add splash screen implementation
    
    * feat(splash-screen): add animation

---
+++ app/src/main/java/com/siloka/client/views/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,48 @@
+package com.siloka.client.views.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import com.siloka.client.R
+import com.siloka.client.databinding.ActivitySplashScreenBinding
+import com.siloka.client.views.main.MainActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setHeader()
+        animateLogo()
+        goToNextPage()
+    }
+
+    private fun setHeader() {
+        supportActionBar?.hide()
+    }
+
+    private fun animateLogo() {
+        val fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        binding.apply {
+            ivTvlkLogo.startAnimation(fadeIn)
+            tvSiloka.startAnimation(fadeIn)
+        }
+    }
+
+    private fun goToNextPage() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
