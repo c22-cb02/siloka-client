5e3e0378a9f8a57d958dfdf4d02b48d0c9767482
Ricky Alverdo <102291249+ClostridiumR@users.noreply.github.com>
2022-06-08 21:16:00 +0700

    Fix: Remove Parcelable and kotlin Parcelize

--- app/src/main/java/com/siloka/client/views/entity/Topic.kt
+++ app/src/main/java/com/siloka/client/views/entity/Topic.kt
@@ -3,9 +3,6 @@ package com.siloka.client.views.entity
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-
-@Parcelize
 data class Topic(
-    var id: Int = 0,
     var question: String,
-): Parcelable
+)
